e97e728cbd8ce87519e92489535b11046b502a47
Ayan Pakhira <120088765+ayan471@users.noreply.github.com>
2025-10-18 13:57:05 +0530

    hid the pricing section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       {/* <Portfolio /> */}
       <Services />
       <Creation />
-      <PricingCheatSheet />
+      {/* <PricingCheatSheet /> */}
       <Satisfaction />
       <Review />
       <Faq />
