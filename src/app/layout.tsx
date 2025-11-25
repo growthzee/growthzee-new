174f709a154cb7861bc5872a2d940eef924c3d08
Ayan Pakhira <120088765+ayan471@users.noreply.github.com>
2025-11-25 13:55:15 +0530

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-// app/layout.tsx
 import { Roboto_Mono, Inter, Jost, Poppins } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import ZoomInfoTracking from "@/components/ZoomInfoTracking";
 
@@ -41,6 +41,21 @@ export default function RootLayout({
       className={`${robotoMono.variable} ${jost.variable} ${inter.variable} ${poppins.variable}`}
     >
       <body>
+        {/* Google Tag Manager (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-50YWN5LE7H"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-50YWN5LE7H');
+          `}
+        </Script>
+
         <ZoomInfoTracking />
         {children}
       </body>
