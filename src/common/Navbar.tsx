dede4db48daefd5b093c2a9855f23c361b660c1b
Ayan Pakhira <120088765+ayan471@users.noreply.github.com>
2025-11-29 14:23:28 +0530

    fixing bugs

--- src/common/Navbar.tsx
+++ src/common/Navbar.tsx
@@ -14,7 +14,7 @@ export default function Navbar() {
   const navItems = useMemo(
     () => [
       { name: "Home", href: "/" },
-      { name: "Case Studies", href: "/portfolio" },
+      { name: "Cases", href: "/portfolio" },
       { name: "Services", href: "/services" },
       { name: "About Us", href: "/about-us" },
       { name: "Team", href: "/teams" },
