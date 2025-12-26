56055d748cdc4cee997f49c07ddedf9fb4bcae96
Ayan Pakhira <120088765+ayan471@users.noreply.github.com>
2025-12-26 11:48:26 +0530

    added meta pixel code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,21 +9,18 @@ const robotoMono = Roboto_Mono({
   variable: "--font-mono",
 });
 
-// Add Jost font for titles
 const jost = Jost({
   weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
   variable: "--font-jost",
 });
 
-// Add a modern sans-serif for clean descriptions
 const inter = Inter({
   weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
   variable: "--font-inter",
 });
 
-// Alternative: Popular modern font
 const poppins = Poppins({
   weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
@@ -41,6 +38,30 @@ export default function RootLayout({
       className={`${robotoMono.variable} ${jost.variable} ${inter.variable} ${poppins.variable}`}
     >
       <body>
+        {/* Meta Pixel Code */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2062040567581835');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2062040567581835&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         {/* Google Tag Manager (gtag.js) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-50YWN5LE7H"
